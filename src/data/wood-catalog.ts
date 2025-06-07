7126295e29a0b160be81484b96a16eab55f38df8
PedroXA <pedroxad@gmail.com>
2025-06-07 18:06:23 -0300

    catalogo de madeiras

---
+++ src/data/wood-catalog.ts
@@ -0,0 +1,75 @@
+export const woodCatalog = [
+    {
+        name: "Noce Mare",
+        category: "Classicas"
+    },
+    {
+        name: "Riviera",
+        category: "Classicas"
+    },
+    {
+        name: "Noce Amêndoa",
+        category: "Classicas"
+    },
+    {
+        name: "Larcana",
+        category: "Classicas",
+
+    },
+    {
+        name: "Amêndola Rústica",
+        category: "Classicas"
+    },
+    {
+        name: "Nogueira Cadiz",
+        category: "Classicas"
+    },
+    {
+        name: "Oásis",
+        category: "Comtemporaneas"
+    },
+    {
+        name: "Carvalho Batur",
+        category: "Comtemporaneas"
+    },
+    {
+        name: "Itapuã",
+        category: "Comtemporaneas"
+    },
+    {
+        name: "Jequitibá Rosa",
+        category: "Comtemporaneas"
+    },
+    {
+        name: "Cumaru Raiz",
+        category: "Comtemporaneas"
+    },
+    {
+        name: "Freijó Puro",
+        category: "Comtemporaneas"
+    },
+    {
+        name: "Nogueira Asti",
+        category: "Comtemporaneas"
+    },
+    {
+        name: "Pau Ferro Natural",
+        category: "Comtemporaneas"
+    },
+    {
+        name: "Bétula",
+        category: "Nobres"
+    },
+    {
+        name: "Rovere Braga",
+        category: "Nobres"
+    },
+    {
+        name: "Carvalho Berlim",
+        category: "Nobres"
+    },
+    {
+        name: "Freijó Imperial",
+        category: "Nobres"
+    }
+];
