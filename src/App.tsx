0b324514d5b9c64da0b131dbb6b0c63e1326153e
PedroXA <pedroxad@gmail.com>
2025-06-07 18:03:46 -0300

    add: formulário de modulos adicionado

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import { Header } from "./components/Header";
+import { Form } from "./components/Form/Form";
+import { Header } from "./components/Header/Header";
 
 function App() {
   return (
     <>
       <Header />
+      <Form />
     </>
   );
 }
