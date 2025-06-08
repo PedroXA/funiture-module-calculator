33b8a475f936c123abde3a8472cf0abc0f17a4c3
PedroXA <pedroxad@gmail.com>
2025-06-08 00:29:05 -0300

    add: Adição dos botões de adicionar módulo e limpar formulário

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -4,12 +4,14 @@ import { ModuleDrawers } from "./fields/Module-drawers";
 import { ModuleMeasures } from "./fields/Module-measures";
 
 import { ModuleShelfs } from "./fields/Module-shelfs";
+import { ModuleComplement } from "./fields/Module-complement";
+import Icons from "../../utils/Icons";
 
 export function Form() {
   return (
     <form
       action=""
-      className="-mt-20 ml-18 mr-18 flex flex-col bg-white w-4/12 pl-12 pr-12 pt-8  rounded-2xl"
+      className="-mt-20 ml-[12rem] mr-[12rem] flex flex-col bg-white w-[32rem] h-[47rem] pl-12 pr-12 pt-8  rounded-2xl"
     >
       <h1 className="text-4xl grid col-span-3">Dados do Módulo</h1>
       <ModuleType />
@@ -17,6 +19,23 @@ export function Form() {
       <ModuleDoors />
       <ModuleDrawers />
       <ModuleShelfs />
+      <ModuleComplement />
+
+      <div className="flex gap-2">
+        <button
+          type="submit"
+          className="w-[13rem] h-[3rem] bg-[#6BFF8B] flex justify-center align-middle p-3.5 gap-2 rounded-2xl mt-5 font-semibold"
+        >
+          <Icons.newModule height={16} /> Adicionar Módulo
+        </button>
+
+        <button
+          type="submit"
+          className="w-[8rem] h-[3rem] bg-[#FFCD29] flex justify-center align-middle p-3.5 gap-2 rounded-2xl mt-5 font-semibold"
+        >
+          <Icons.clear height={16} /> Limpar
+        </button>
+      </div>
     </form>
   );
 }
