43be6d01f9be9059a20cd8473b35056dd57e4010
PedroXA <pedroxad@gmail.com>
2025-06-10 17:16:38 -0300

    changed: pequenos ajustes de css

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,6 @@
 export function Header() {
   return (
-    <header className="p-16 pl-[12rem] pr-[12rem] bg-red-background">
+    <header className="p-16 pl-[8rem] pr-[8rem] bg-red-background">
       <h3 className="text-white pb-16">Calculadora de Módulos</h3>
     </header>
   );
