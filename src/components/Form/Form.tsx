b826aa9d4809223c1752a079910195997bacffcc
PedroXA <pedroxad@gmail.com>
2025-06-11 16:07:08 -0300

    feat: Implement form handling with validation using react-hook-form and zod

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -7,19 +7,46 @@ import { ModuleShelfs } from "./fields/Module-shelfs";
 import { ModuleComplement } from "./fields/Module-complement";
 import Icons from "../../utils/Icons";
 
+import { useForm, type FieldValues } from "react-hook-form";
+import { useState } from "react";
+import { z } from "zod";
+
 export function Form() {
+  const [output, setOutput] = useState("");
+  const { register, handleSubmit } = useForm<FieldValues>();
+
+  function createModule(data: any) {
+    setOutput(JSON.stringify(data, null, 2));
+  }
+
+  // Module Schema
+  const createModuleFormSchema = z.object({
+    moduleName: z.string().nonempty("O módulo precisa ter um nome."),
+    moduleWoodType: z.string(),
+    largura: z.number().int(),
+    altura: z.number().int(),
+    profundidade: z.number().int(),
+    moduleDoorType: z.string(),
+    moduleDoorQuantity: z.number().int(),
+    moduleDrawerType: z.string().nonempty(),
+    moduleDrawerQuantity: z.number().int(),
+    moduleShelfQuantity: z.number().int(),
+    moduleComplement: z.string(),
+  });
+
   return (
     <form
+      onSubmit={handleSubmit(createModule)}
       action=""
       className="-mt-20 ml-[8rem] mr-[8rem] flex flex-col bg-white w-[32rem] h-[37rem] p-8 rounded-2xl"
     >
       <h1 className="text-4xl">Dados do Módulo</h1>
-      <ModuleType />
-      <ModuleMeasures />
-      <ModuleDoors />
-      <ModuleDrawers />
-      <ModuleShelfs />
-      <ModuleComplement />
+      <ModuleType register={register} />
+      <ModuleMeasures register={register} />
+      <ModuleDoors register={register} />
+      <ModuleDrawers register={register} />
+      <ModuleShelfs register={register} />
+      <ModuleComplement register={register} />
 
       <div className="flex gap-2">
         <button
@@ -36,6 +63,8 @@ export function Form() {
           <Icons.clear height={16} /> Limpar
         </button>
       </div>
+
+      <pre>{output}</pre>
     </form>
   );
 }
