e4d7f3f36c92ee02d079a709160bfa06992df3e7
PedroXA <pedroxad@gmail.com>
2025-06-16 16:16:21 -0300

    refactor: Simplify form structure by integrating fields directly and removing unused components

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,28 +1,15 @@
-import { ModuleType } from "./fields/Module-type";
-import { ModuleDoors } from "./fields/Module-doors";
-import { ModuleDrawers } from "./fields/Module-drawers";
-import { ModuleMeasures } from "./fields/Module-measures";
-
-import { ModuleShelfs } from "./fields/Module-shelfs";
-import { ModuleComplement } from "./fields/Module-complement";
 import Icons from "../../utils/Icons";
 
 import { useForm, type FieldValues } from "react-hook-form";
 import { useState } from "react";
 import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 export function Form() {
-  const [output, setOutput] = useState("");
-  const { register, handleSubmit } = useForm<FieldValues>();
-
-  function createModule(data: any) {
-    setOutput(JSON.stringify(data, null, 2));
-  }
-
   // Module Schema
   const createModuleFormSchema = z.object({
     moduleName: z.string().nonempty("O módulo precisa ter um nome."),
-    moduleWoodType: z.string(),
+    moduleWoodType: z.string().nonempty("Selecione um tipo de Madeira"),
     largura: z.number().int(),
     altura: z.number().int(),
     profundidade: z.number().int(),
@@ -34,6 +21,19 @@ export function Form() {
     moduleComplement: z.string(),
   });
 
+  const [output, setOutput] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(createModuleFormSchema),
+  });
+
+  function createModule(data: any) {
+    setOutput(JSON.stringify(data, null, 2));
+  }
+
   return (
     <form
       onSubmit={handleSubmit(createModule)}
@@ -41,12 +41,144 @@ export function Form() {
       className="-mt-20 ml-[8rem] mr-[8rem] flex flex-col bg-white w-[32rem] h-[37rem] p-8 rounded-2xl"
     >
       <h1 className="text-4xl">Dados do Módulo</h1>
-      <ModuleType register={register} />
-      <ModuleMeasures register={register} />
-      <ModuleDoors register={register} />
-      <ModuleDrawers register={register} />
-      <ModuleShelfs register={register} />
-      <ModuleComplement register={register} />
+      <div className="flex flex-row">
+        <div className="basis-2/3">
+          <label htmlFor="moduleName">Nome do Módulo</label>
+          <input
+            type="text"
+            // {...register("moduleName")}
+            id="moduleName"
+            className="h-12 w-[16rem] p-2 bg-[#f3f3f3]! rounded-r-md"
+          />
+        </div>
+
+        <div className="basis-1/3">
+          <label htmlFor="module-wood-type">Tipo da Madeira</label>
+
+          {/* TO-DO: Linkar a database de madeiras com o select */}
+          <select
+            // {...register("woodType")}
+            id="woodType"
+            className="h-12 w-[8rem] bg-[#f3f3f3]! rounded-r-md"
+          >
+            <option value="">Selecione um tipo de Madeira</option>
+            <option value="option1">Opcao 1</option>
+            <option value="option2">Opcao 2</option>
+            <option value="option3">Opcao 3</option>
+          </select>
+        </div>
+      </div>
+
+      <div className="flex">
+        <label className="basis-1/3">
+          Largura <span className="text-red-600 text-xs">cm²</span>
+          <input
+            type="number"
+            // {...register("largura")}
+            id="largura"
+            className="h-12 w-[6.5rem] p-2 bg-[#f3f3f3]! rounded-r-md"
+          />
+        </label>
+
+        <label className="basis-1/3">
+          Altura <span className="text-red-600 text-xs">cm²</span>
+          <input
+            type="number"
+            // {...register("altura")}
+            id="altura"
+            className="h-12 w-[6.5rem] p-2 bg-[#f3f3f3]! rounded-r-md"
+          />
+        </label>
+
+        <label className="basis-1/3">
+          Profundidade <span className="text-red-600 text-xs">cm²</span>
+          <input
+            type="number"
+            // {...register("profundidade")}
+            id="profundidade"
+            className="h-12 w-[8rem] p-2 bg-[#f3f3f3]! rounded-r-md"
+          />
+        </label>
+      </div>
+
+      <div className="flex flex-row">
+        <label className="basis-2/3">
+          Tipo da Porta
+          <select
+            {...register("moduleDoorType")}
+            id="doorType"
+            className="h-12 w-[16rem] bg-[#f3f3f3]! rounded-r-md"
+          >
+            <option value="">Seletione o tipo de Porta</option>
+            <option value="Sem portas">Sem Portas</option>
+            <option value="correr">Correr - R$ 70 Un</option>
+            {/* Basculante - Pistão */}
+            <option value="basculante">Basculante - R$ 30 Un</option>
+          </select>
+        </label>
+
+        <label className="basis-1/3">
+          Quantidade
+          <input
+            type="number"
+            {...register("moduleDoorQuantity")}
+            id="doorQuantity"
+            className="h-12 w-[8rem] p-2 bg-[#f3f3f3]! rounded-r-md"
+          />
+        </label>
+      </div>
+
+      <div className="flex flex-row">
+        <label className="basis-2/3">
+          Tipo da Gaveta
+          <select
+            {...register("moduleDrawerType")}
+            id="drawers"
+            className="h-12 w-[16rem] bg-[#f3f3f3]! rounded-r-md"
+          >
+            <option value="semGavetas">Sem Gavetas</option>
+            <option value="interna">Interna</option>
+            <option value="externa">Externa</option>
+          </select>
+        </label>
+
+        <label className="basis-1/3">
+          Quantidade
+          <input
+            type="number"
+            {...register("moduleDrawerQuantity")}
+            id="drawersQuantity"
+            className="h-12 w-[8rem] p-2 bg-[#f3f3f3]! rounded-r-md"
+          />
+        </label>
+      </div>
+
+      <div className="flex flex-col">
+        <p>Quantidade de Prateleiras</p>
+        <input
+          type="text"
+          {...register("moduleShelfQuantity")}
+          id="shelfQuantity"
+          className="h-12 w-[8rem] p-2  bg-[#f3f3f3]! rounded-r-md"
+          placeholder="0"
+        />
+      </div>
+
+      <div className="flex flex-col">
+        <p>
+          Acabamento <span className="text-red-600">*</span>
+        </p>
+        <select
+          {...register("moduleComplement")}
+          id="complement"
+          className="h-12 w-[16rem] bg-[#f3f3f3]! rounded-r-md"
+        >
+          <option value="semAcabamento">Selecione o Acabamento</option>
+          <option value="option1">Opcao 1</option>
+          <option value="option2">Opcao 2</option>
+          <option value="option3">Opcao 3</option>
+        </select>
+      </div>
 
       <div className="flex gap-2">
         <button
