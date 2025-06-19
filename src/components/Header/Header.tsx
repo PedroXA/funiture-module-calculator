5a7b24bd5eb98f0eb59cfa6de459266fe1cd635a
PedroXA <pedroxad@gmail.com>
2025-06-19 19:57:22 -0300

    refactor: Remove unused Header component

--- src/components/Header/Header.tsx
+++
@@ -1,7 +0,0 @@
-export function Header() {
-  return (
-    <header className="p-16 pl-[8rem] pr-[8rem] bg-red-background">
-      <h3 className="text-white pb-16">Calculadora de Módulos</h3>
-    </header>
-  );
-}
